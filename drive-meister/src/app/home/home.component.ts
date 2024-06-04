fbd8b4b837edc221ec79de166adad74eccd76114
suzukikeita1 <keita151105@gmail.com>
2024-06-04 15:08:44 +0900

    コンポーネントを追加

--- drive-meister/src/app/home/home.component.ts
+++ drive-meister/src/app/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { HomeAnswerComponent } from '../home-answer/home-answer.component';
+import { HomeReviewComponent } from '../home-review/home-review.component';
+import { HomeHeaderComponent } from '../home-header/home-header.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    HomeAnswerComponent,
+    HomeReviewComponent,
+    HomeHeaderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
