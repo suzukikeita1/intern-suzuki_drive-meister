a1d54ef9402e0f1c8412dab356e57c93f73d05e4
suzukikeita1 <keita151105@gmail.com>
2024-06-18 23:13:02 +0900

    wip

--- drive-meister/src/app/login/login.component.ts
+++ drive-meister/src/app/login/login.component.ts
@@ -16,16 +16,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { merge } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { LoginEmailComponent } from '../login-email/login-email.component';
-import { LoginPasswordComponent } from '../login-password/login-password.component';
-import { LoginSubmitComponent } from '../login-submit/login-submit.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    LoginEmailComponent,
-    LoginSubmitComponent,
-    LoginPasswordComponent,
     MyPageLoginLogoutComponent,
     ReturnHomeButtonComponent,
     MatButtonModule,
@@ -41,4 +36,53 @@ import { LoginSubmitComponent } from '../login-submit/login-submit.component';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  hide = true;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern(
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$'
+    ),
+  ]);
+
+  emailerrorMessage = '';
+  passworderrorMessage = '';
+
+  constructor() {
+    merge(
+      this.email.statusChanges,
+      this.email.valueChanges,
+      this.password.statusChanges,
+      this.password.valueChanges
+    )
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessages());
+  }
+
+  updateErrorMessages() {
+    this.passworderrorMessage = this.getErrorMessage(this.password, {
+      required: 'この項目は必須です',
+      minlength: '8文字以上で入力してください',
+      pattern: '大文字、小文字、数字、記号をそれぞれ1文字以上含めてください',
+    });
+
+    this.emailerrorMessage = this.getErrorMessage(this.email, {
+      required: 'この項目は必須です',
+      email: 'メールアドレスの形式が正しくありません',
+    });
+  }
+
+  getErrorMessage(
+    control: FormControl,
+    errorMessages: { [key: string]: string }
+  ): string {
+    if (control.errors) {
+      const errorKey = Object.keys(control.errors)[0];
+      return errorMessages[errorKey] || '';
+    }
+    return '';
+  }
+}
